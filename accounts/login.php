22f7ac357ddf97356b6507c176e8b7d32818f322
codytyerman <codytyerman99@outlook.com>
2022-01-12 05:07:50 -0800

    added signup/reg pages and file upload feature

---
+++ accounts/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>WooWooNetwork</title>
+    </head>
+    <body>
+        <h3>Login to find a healer today!</h3>
+        <form>
+            <label>Email</label>
+            <input type="text"><br>
+            <label>Password</label>
+            <input type="text"><br>
+            <a href="/register.php">New? Click to register</a><br>
+            <input type="submit" value="login">
+        </form>
+    </body>
+</html>
---
+++ accounts/register.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>WooWooNetwork</title>
+    </head>
+    <body>
+        <h3>register as</h3>
+        <button>client</button>
+        <button>healer</button>
+        <p>sign up is free for both clients and healers</p>
+    </body>
+</html>
