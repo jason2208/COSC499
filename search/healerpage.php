ab84e283e758544267eeadd37dacf4c45bf3e3c7
codytyerman <codytyerman99@outlook.com>
2022-01-18 05:39:20 -0800

    finished frontend prototype search frontend layout

---
+++ search/healerpage.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>WooWooNetwork</title>
+    </head>
+    <body>
+        <p>healer page</p>
+    </body>
+</html>
--- search/scripts/scripts.js
+++ search/scripts/scripts.js
@@ -1,22 +1,22 @@
-var x = document.getElementById("demo");
-//var location;
+window.onload = function() 
+{
+    var x = document.getElementById("demo");
+    getLocation();
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
+    function getLocation() 
+    {
+        if (navigator.geolocation) 
+        {
+            navigator.geolocation.getCurrentPosition(showPosition);
+        } 
+        else 
+        { 
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
     }
-}
 
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-    /*
-    location = { 
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-    };
-    console.log(location.latitude);
-    console.log(location.longitude);
-    */
+    function showPosition(position) 
+    {
+        x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+    }
 }
