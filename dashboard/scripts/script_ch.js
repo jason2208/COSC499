ae0fdb86551128e4b160631d04cdd8858ead9308
jason2208 <jason22320202@gmail.com>
2022-01-21 16:46:33 -0800

    Update
    Update a lot of places in both files using Jquery and added comments. Also fixed the filter function.

--- dashboard/scripts/script_ch.js
+++ dashboard/scripts/script_ch.js
@@ -1,28 +1,30 @@
 window.onload = function() {
 
     //get elements from html form
-    var table = document.getElementById("table-results");
-    var btnClear = document.getElementById("btn-clear");
-    var btnEnable = document.getElementById("btn-enable");
-    var btnDisable = document.getElementById("btn-disable");
-    var btnRemove = document.getElementById("btn-remove");
-    var selectVisibilityFilter = document.getElementById("select-visibility-filter");
+    var table = $('#table-results');
+    var btnClear = $('#btn-clear');
+    var btnEnable = $("#btn-enable");
+    var btnDisable = $("#btn-disable");
+    var btnRemove =  $("#btn-remove");
+    //var selectVisibilityFilter = $("#select-visibility-filter");
 
-    btnClear.addEventListener("click", clear, false);
-    btnEnable.addEventListener("click", putCheckedIDsInEnableForm, false);
-    btnDisable.addEventListener("click", putCheckedIDsInDisableForm, false);
-    btnRemove.addEventListener("click", putCheckedIDsInRemoveForm, false);
-    selectVisibilityFilter.addEventListener("change", filter, false);
+    
 
-    function clear() {
-        for (var i = 1, row; row = table.rows[i]; i++) {
-            var checkbox = row.cells[0].childNodes[0];
-            checkbox.checked = false; 
+    //clear checkbox in the table on the left
+    btnClear.on('click',function(){
+        //find last index of table
+        last=table.find('tr:last').index();
+        //uncheck box
+        for(var i=1;i<=last+1;i++){
+            $('#checkbox'+i).prop('checked',false);
         }
-    }
+    })
 
-    function putCheckedIDsInEnableForm() {
-        var hidden = document.getElementById("hiddenEnable");
+
+
+    //To enable user
+    btnEnable.on('click',function(){
+        var hidden = $('#hiddenEnable');
         var checkedIDs = getCheckedIDs();
         var result = "";
         for (var i = 0; i < checkedIDs.length; i++) {
@@ -31,11 +33,13 @@ window.onload = function() {
                 result += ",";
             }
         }
-        hidden.value = result;
-    }
+        hidden.val(result);
+    })
 
-    function putCheckedIDsInDisableForm() {
-        var hidden = document.getElementById("hiddenDisable");
+
+    //To disable user
+    btnDisable.on('click',function(){
+        var hidden = $('#hiddenDisable');
         var checkedIDs = getCheckedIDs();
         var result = "";
         for (var i = 0; i < checkedIDs.length; i++) {
@@ -44,11 +48,15 @@ window.onload = function() {
                 result += ",";
             }
         }
-        hidden.value = result;
-    }
+        hidden.val(result);
+    })
 
-    function putCheckedIDsInRemoveForm() {
-        var hidden = document.getElementById("hiddenRemove");
+
+
+
+    //To remove a user
+    btnRemove.on('click',function(){
+        var hidden = $('#hiddenRemove');
         var checkedIDs = getCheckedIDs();
         var result = "";
         for (var i = 0; i < checkedIDs.length; i++) {
@@ -57,48 +65,59 @@ window.onload = function() {
                 result += ",";
             }
         }
+        //Confirm with admin if actually want to remove
         if (result != "") {
             var txt;
             var r = confirm("Are you sure you want to remove the rows with the following IDs?\n" + result);
             if (r == true) {
                 txt = "true";
-                hidden.value = result;
+                hidden.val(result);
             } else {
                 txt = "false";
-                hidden.value = "";
+                hidden.val("");
             }
         } else {
             alert("You must select rows before deletion");
         }
-    }
+    })
 
+
+
+    // Get IDs where the checkbox is checked
     function getCheckedIDs() {
         var checked = [];
-        for (var i = 1, row; row = table.rows[i]; i++) {
-            var checkbox = row.cells[0].childNodes[0];
-            if (checkbox.checked) {
-                var id = row.cells[1].innerHTML;
-                checked.push(id);
+        last=table.find('tr:last').index();
+        //loop through each row to see if checked
+        for(var i=1;i<=last+1;i++){
+            if($('#checkbox'+i).is(':checked')){
+                checked.push(i);
             }
         }
         return checked;
     }
 
-    function filter() {
-        var text = selectVisibilityFilter.options[selectVisibilityFilter.selectedIndex].text;
-        if (text == "Show All") {
-            for (var i = 1, row; row = table.rows[i]; i++) {
-                row.style.display = "";
-            }
-        } else {
-            for (var i = 1, row; row = table.rows[i]; i++) {
-                var status = row.cells[6];
-                if (status.innerHTML != text) {
-                    row.style.display = "none";
-                } else {
-                    row.style.display = "";
-                }
-            }
-        }
-    }
+
+    //Logic is weird so correct version in client,php
+    //still modified and kept this version
+    //check which is selected (Show all, Enabled, Disabled)
+    // selectVisibilityFilter.change(function(){
+    //     //Variable text is the selected option
+    //     var text = selectVisibilityFilter.val();
+    //     if (text == "Show All") {
+    //         for (var i = 1; i<=table.find('tr:last').index()+1; i++) {
+    //             $('#row-data'+i).css('display',"''");
+    //         }
+    //     } else {
+    //         for (var i = 0; i<table.find('tr:last').index()+1; i++) {
+    //             var status = table.find('tr').find('td:eq(6)')[i];
+    //             var rownum=1;
+    //             if (status.innerHTML == text) {
+    //                 $('#row-data'+rownum).css('display',"''");
+    //             } else {
+    //                 $('#row-data'+rownum).css('display',"none");
+    //             }
+    //             rownum++;
+    //         }
+    //     }
+    // })
 }
