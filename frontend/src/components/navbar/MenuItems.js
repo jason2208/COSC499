50c1bf40a11c7c8138dad9bcb90f2cd2a8c0711b
codytyerman <codytyerman99@outlook.com>
2022-02-15 11:25:28 -0800

    cleaned repo

---
+++ frontend/src/components/navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems=[
+    {
+        title:'Find Healers',
+        url:'search',
+        cName: 'nav-links'
+    },
+    {
+        title:'Services',
+        url:'./services',
+        cName: 'nav-links'
+    },
+    {
+        title:'About',
+        url:'./about',
+        cName: 'nav-links'
+    },
+]
---
+++ frontend/src/components/pages/services.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Button} from '../Button/button';
+
+class Services extends React.Component{
+    render(){
+        return(
+        <div className='Services'>
+            <div className="container">
+               
+            </div>
+        </div>
+
+
+);
+}
+}
+
+export default Services
