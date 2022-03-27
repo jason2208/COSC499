443e5f6043a20339886701083d26b3db0f78c497
Cassie <cassandraptrs@gmail.com>
2022-03-27 17:11:51 -0400

    Sequalize API Image and User routes defined and working

---
+++ nodejs-express-sequelize-mysql/app/controllers/image.controller.js
@@ -0,0 +1,115 @@
+const db = require("../models");
+const Image = db.image;
+const Op = db.Sequelize.Op;
+
+
+// Retrieve all images from the database where uid is
+exports.findAll = (req, res) => {
+    const uid = req.query.uid;
+    const type = req.query.type;
+    var condition = uid ? { 
+            uid: { [Op.like]: `%${uid}%` },
+            name: { [Op.like]: `${type}%` }
+    } : null;
+    Image.findAll({ where: condition })
+      .then(data => {
+        res.send(data);
+      })
+     .then(data => {
+        console.log("what the fuck");
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving uesers."
+      });
+    });
+};
+// Find a single user with an id
+exports.findOne = (req, res) => {
+    const uid = req.query.uid;
+    const type = req.query.type;
+    var condition = uid ? { 
+            uid: { [Op.like]: `%${uid}%` },
+            name: { [Op.like]: `${type}%` }
+    } : null;
+  Image.findOne(condition)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find user with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving user with id=" + id
+      });
+    });
+};
+// Update a user by the id in the request
+exports.update = (req, res) => {
+  const uid = req.query.uid;
+  Image.update(req.query, {
+    where: { uid: uid }
+
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "User was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update User with id=${id}. Maybe user was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating user with id=" + id
+      });
+    });
+};
+// Delete a user with the specified id in the request
+exports.delete = (req, res) => {
+  const uid = req.params.uid;
+  Image.destroy({
+    where: { uid: uid }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "User was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete user with id=${id}. Maybe user was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete user with id=" + id
+      });
+    });
+};
+// Delete all users from the database.
+exports.deleteAll = (req, res) => {
+  Image.destroy({
+    where: {},
+    truncate: false
+  })
+    .then(nums => {
+      res.send({ message: `${nums} Users were deleted successfully!` });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all Users."
+      });
+    });
+};
